5b64093cd4623586d967a82eed52f1a85de8e0ae
Максим Пушкарев <maxim.pushkarev@playrix.com>
2024-11-24 18:54:46 +0400

    dx12 base entity

--- src/Backend/Dx12/Entity.cpp
+++ src/Backend/Dx12/Entity.cpp
@@ -2,5 +2,21 @@
 
 namespace MMPEngine::Backend::Dx12
 {
-	
+	BaseEntity::BaseEntity(const Microsoft::WRL::ComPtr<ID3D12Resource>& nativeResource) : _nativeResource(nativeResource)
+	{
+		SetUpNativeResourceName();
+	}
+
+	BaseEntity::BaseEntity(const Microsoft::WRL::ComPtr<ID3D12Resource>& nativeResource, std::string_view name) : Core::BaseEntity(name), _nativeResource(nativeResource)
+	{
+		SetUpNativeResourceName();
+	}
+
+	void BaseEntity::SetUpNativeResourceName()
+	{
+		const auto str = GetName();
+		std::wstring wName(std::begin(str), std::end(str));
+		_nativeResource->SetName(wName.c_str());
+	}
+
 }
--- src/Backend/Dx12/Entity.hpp
+++ src/Backend/Dx12/Entity.hpp
@@ -1,6 +1,18 @@
 #pragma once
+#include <d3d12.h>
+#include <Core/Entity.hpp>
+#include <wrl/client.h>
 
 namespace MMPEngine::Backend::Dx12
 {
-
+	class BaseEntity : Core::BaseEntity
+	{
+	protected:
+		BaseEntity(const Microsoft::WRL::ComPtr<ID3D12Resource>& nativeResource, std::string_view name);
+		BaseEntity(const Microsoft::WRL::ComPtr<ID3D12Resource>& nativeResource);
+	private:
+		void SetUpNativeResourceName();
+	protected:
+		Microsoft::WRL::ComPtr<ID3D12Resource> _nativeResource;
+	};
 }
