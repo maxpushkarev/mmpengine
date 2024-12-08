8de58702a2e0a1627a904124bafcfecc320629c7
Максим Пушкарев <maxim.pushkarev@playrix.com>
2024-12-08 18:23:00 +0400

    created: parameters

--- src/Core/Material.cpp
+++ src/Core/Material.cpp
@@ -1,6 +1,67 @@
 #include <Core/Material.hpp>
+#include <cassert>
 
 namespace MMPEngine::Core
 {
+	BaseMaterial::Parameters::Parameters()
+	{
+		Build();
+	};
+
+	BaseMaterial::Parameters::~Parameters() = default;
+
+
+	BaseMaterial::Parameters::Parameters(std::vector<Entry>&& entries) : _entries(std::move(entries))
+	{
+		Build();
+	}
+
+	BaseMaterial::Parameters::Parameters(const Parameters& other) : _entries(other._entries)
+	{
+		Build();	
+	}
+
+	BaseMaterial::Parameters::Parameters(Parameters&& other) noexcept
+	{
+		if(this != &other)
+		{
+			_entries = std::move(other._entries);
+			other._viewMap.clear();
+
+			Build();
+		}
+	}
+
+	const std::vector<BaseMaterial::Parameters::Entry>& BaseMaterial::Parameters::GetAll() const
+	{
+		return _entries;
+	}
+
+	std::optional<BaseMaterial::Parameters::EntryView> BaseMaterial::Parameters::TryGet(std::string_view name) const
+	{
+		if(_viewMap.find(name) != _viewMap.cend())
+		{
+			return _viewMap.at(name);
+		}
+
+		return std::nullopt;
+	}
+
+	void BaseMaterial::Parameters::Build()
+	{
+		for (std::size_t i = 0; i < _entries.size(); ++i)
+		{
+			const auto& entry = _entries[i];
+			const auto& name = entry.name;
+
+			assert(_viewMap.find(name) == _viewMap.cend());
+
+			EntryView entryView{};
+			entryView.entryPtr = &entry;
+			entryView.index = static_cast<decltype(entryView.index)>(i);
+
+			_viewMap.emplace(std::make_pair(name, entryView));
+		}
+	}
 
 }
--- src/Core/Material.hpp
+++ src/Core/Material.hpp
@@ -1,6 +1,54 @@
 #pragma once
+#include <optional>
+#include <Core/Base.hpp>
+#include <Core/Entity.hpp>
 
 namespace MMPEngine::Core
 {
+	class BaseMaterial : public IInitializationTaskSource
+	{
+	public:
+		class Parameters final
+		{
+		public:
+			struct Entry final
+			{
+				std::string name;
+				std::shared_ptr<BaseEntity> entity;
+			};
 
+			struct EntryView final
+			{
+				const Entry* entryPtr;
+				std::uint32_t index;
+			};
+
+			Parameters(std::vector<Entry>&& entries);
+			Parameters();
+			~Parameters();
+
+			Parameters(const Parameters&);
+			Parameters(Parameters&&) noexcept;
+
+			Parameters& operator=(const Parameters&) = delete;
+			Parameters& operator=(Parameters&&) noexcept = delete;
+
+			std::optional<EntryView> TryGet(std::string_view name) const;
+			const std::vector<Entry>& GetAll() const;
+		private:
+
+			void Build();
+
+			std::vector<Entry> _entries;
+			std::unordered_map<std::string_view, EntryView> _viewMap;
+		};
+	};
+
+	class MeshMaterial : public BaseMaterial
+	{
+	};
+
+	class ComputeMaterial : public BaseMaterial
+	{
+	};
 }
