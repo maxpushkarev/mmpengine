defed8eda271c7309228944951da32a41ad2d750
Максим Пушкарев <maxim.pushkarev@playrix.com>
2024-12-14 17:56:30 +0400

    math fix

--- src/Frontend/App.cpp
+++ src/Frontend/App.cpp
@@ -9,16 +9,12 @@ namespace MMPEngine::Frontend
 	std::shared_ptr<Core::BaseRootApp> App::BuildRootApp(
 		const Core::AppContext::Settings& appContextSettings, 
 		const std::shared_ptr<Core::UserApp>& userApp,
-		std::unique_ptr<Core::Math>&& math,
 		std::unique_ptr<Core::BaseLogger>&& logger)
 	{
 		if(appContextSettings.backend == Core::BackendType::Dx12)
 		{
 #ifdef MMPENGINE_BACKEND_DX12
-			if(!math)
-			{
-				math = std::make_unique<Backend::Dx12::DxMath>();
-			}
+			auto math = std::make_unique<Backend::Dx12::DxMath>();
 			const auto rootApp = std::make_shared<Backend::Dx12::RootApp>(std::make_shared<Backend::Dx12::AppContext>(appContextSettings, std::move(math), std::move(logger)));
 			rootApp->Attach(userApp);
 			return rootApp;
