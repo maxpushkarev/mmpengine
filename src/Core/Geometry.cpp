52a0cd80db2705059b1e887d8a3724d77784df75
Максим Пушкарев <maxim.pushkarev@playrix.com>
2024-12-18 18:15:24 +0400

    init geometry + heap

--- src/Core/Geometry.cpp
+++ src/Core/Geometry.cpp
@@ -7,9 +7,251 @@ namespace MMPEngine::Core
 	}
 
 	BaseGeometryBufferPrototype::~BaseGeometryBufferPrototype() = default;
+	BaseGeometryBufferPrototype::BaseGeometryBufferPrototype(BaseGeometryBufferPrototype&&) noexcept = default;
+	BaseGeometryBufferPrototype& BaseGeometryBufferPrototype::operator=(BaseGeometryBufferPrototype&&) noexcept = default;
+
 
 	const BaseGeometryBufferPrototype::Settings& BaseGeometryBufferPrototype::GetSettings() const
 	{
 		return _settings;
 	}
+
+	VertexBufferPrototype::VertexBufferPrototype(const VBSettings& vb) : _vb(vb)
+	{
+	}
+
+	const VertexBufferPrototype::VBSettings& VertexBufferPrototype::GetVBSettings() const
+	{
+		return _vb;
+	}
+
+	VertexBufferPrototypeFloat1::VertexBufferPrototypeFloat1(const Settings& settings) :
+		BaseGeometryBufferPrototype(settings.base),
+		VertexBufferPrototype(settings.vb),
+		GeometryBufferPrototype(settings.base)
+	{
+	}
+
+	VertexBufferPrototype::Format VertexBufferPrototypeFloat1::GetFormat() const
+	{
+		return Format::Float1;
+	}
+
+	std::size_t VertexBufferPrototypeFloat1::GetByteLength() const
+	{
+		return GeometryBufferPrototype::GetByteLength();
+	}
+
+	const void* VertexBufferPrototypeFloat1::GetDataPtr() const
+	{
+		return GeometryBufferPrototype::GetDataPtr();
+	}
+
+	std::size_t VertexBufferPrototypeFloat1::GetElementsCount() const
+	{
+		return GeometryBufferPrototype::GetElementsCount();
+	}
+
+	std::size_t VertexBufferPrototypeFloat1::GetStride() const
+	{
+		return GeometryBufferPrototype::GetStride();
+	}
+
+	VertexBufferPrototypeFloat2::VertexBufferPrototypeFloat2(const Settings& settings) :
+		BaseGeometryBufferPrototype(settings.base),
+		VertexBufferPrototype(settings.vb),
+		GeometryBufferPrototype(settings.base)
+	{
+	}
+
+	VertexBufferPrototype::Format VertexBufferPrototypeFloat2::GetFormat() const
+	{
+		return Format::Float2;
+	}
+
+	std::size_t VertexBufferPrototypeFloat2::GetByteLength() const
+	{
+		return GeometryBufferPrototype::GetByteLength();
+	}
+
+	const void* VertexBufferPrototypeFloat2::GetDataPtr() const
+	{
+		return GeometryBufferPrototype::GetDataPtr();
+	}
+
+	std::size_t VertexBufferPrototypeFloat2::GetElementsCount() const
+	{
+		return GeometryBufferPrototype::GetElementsCount();
+	}
+
+	std::size_t VertexBufferPrototypeFloat2::GetStride() const
+	{
+		return GeometryBufferPrototype::GetStride();
+	}
+
+	VertexBufferPrototypeFloat3::VertexBufferPrototypeFloat3(const Settings& settings) :
+		BaseGeometryBufferPrototype(settings.base),
+		VertexBufferPrototype(settings.vb),
+		GeometryBufferPrototype(settings.base)
+	{
+	}
+
+	VertexBufferPrototype::Format VertexBufferPrototypeFloat3::GetFormat() const
+	{
+		return Format::Float3;
+	}
+
+	std::size_t VertexBufferPrototypeFloat3::GetByteLength() const
+	{
+		return GeometryBufferPrototype::GetByteLength();
+	}
+
+	const void* VertexBufferPrototypeFloat3::GetDataPtr() const
+	{
+		return GeometryBufferPrototype::GetDataPtr();
+	}
+
+	std::size_t VertexBufferPrototypeFloat3::GetElementsCount() const
+	{
+		return GeometryBufferPrototype::GetElementsCount();
+	}
+
+	std::size_t VertexBufferPrototypeFloat3::GetStride() const
+	{
+		return GeometryBufferPrototype::GetStride();
+	}
+
+	VertexBufferPrototypeFloat4::VertexBufferPrototypeFloat4(const Settings& settings) :
+		BaseGeometryBufferPrototype(settings.base),
+		VertexBufferPrototype(settings.vb),
+		GeometryBufferPrototype(settings.base)
+	{
+	}
+
+	VertexBufferPrototype::Format VertexBufferPrototypeFloat4::GetFormat() const
+	{
+		return Format::Float4;
+	}
+
+	std::size_t VertexBufferPrototypeFloat4::GetByteLength() const
+	{
+		return GeometryBufferPrototype::GetByteLength();
+	}
+
+	const void* VertexBufferPrototypeFloat4::GetDataPtr() const
+	{
+		return GeometryBufferPrototype::GetDataPtr();
+	}
+
+	std::size_t VertexBufferPrototypeFloat4::GetElementsCount() const
+	{
+		return GeometryBufferPrototype::GetElementsCount();
+	}
+
+	std::size_t VertexBufferPrototypeFloat4::GetStride() const
+	{
+		return GeometryBufferPrototype::GetStride();
+	}
+
+	VertexBufferPrototypeUint4::VertexBufferPrototypeUint4(const Settings& settings) :
+		BaseGeometryBufferPrototype(settings.base),
+		VertexBufferPrototype(settings.vb),
+		GeometryBufferPrototype(settings.base)
+	{
+	}
+
+	VertexBufferPrototype::Format VertexBufferPrototypeUint4::GetFormat() const
+	{
+		return Format::Uint4;
+	}
+
+	std::size_t VertexBufferPrototypeUint4::GetByteLength() const
+	{
+		return GeometryBufferPrototype::GetByteLength();
+	}
+
+	const void* VertexBufferPrototypeUint4::GetDataPtr() const
+	{
+		return GeometryBufferPrototype::GetDataPtr();
+	}
+
+	std::size_t VertexBufferPrototypeUint4::GetElementsCount() const
+	{
+		return GeometryBufferPrototype::GetElementsCount();
+	}
+
+	std::size_t VertexBufferPrototypeUint4::GetStride() const
+	{
+		return GeometryBufferPrototype::GetStride();
+	}
+
+	IndexBufferPrototype16::IndexBufferPrototype16(const Settings& settings) :
+		BaseGeometryBufferPrototype(settings),
+		IndexBufferPrototype(),
+		GeometryBufferPrototype(settings)
+	{
+	}
+
+	IndexBufferPrototype::Format IndexBufferPrototype16::GetFormat() const
+	{
+		return Format::Uint16;
+	}
+
+	std::size_t IndexBufferPrototype16::GetByteLength() const
+	{
+		return GeometryBufferPrototype::GetByteLength();
+	}
+
+	const void* IndexBufferPrototype16::GetDataPtr() const
+	{
+		return GeometryBufferPrototype::GetDataPtr();
+	}
+
+	std::size_t IndexBufferPrototype16::GetElementsCount() const
+	{
+		return GeometryBufferPrototype::GetElementsCount();
+	}
+
+	std::size_t IndexBufferPrototype16::GetStride() const
+	{
+		return GeometryBufferPrototype::GetStride();
+	}
+
+	IndexBufferPrototype32::IndexBufferPrototype32(const Settings& settings) :
+		BaseGeometryBufferPrototype(settings),
+		IndexBufferPrototype(),
+		GeometryBufferPrototype(settings)
+	{
+	}
+
+	IndexBufferPrototype::Format IndexBufferPrototype32::GetFormat() const
+	{
+		return Format::Uint32;
+	}
+
+	std::size_t IndexBufferPrototype32::GetByteLength() const
+	{
+		return GeometryBufferPrototype::GetByteLength();
+	}
+
+	const void* IndexBufferPrototype32::GetDataPtr() const
+	{
+		return GeometryBufferPrototype::GetDataPtr();
+	}
+
+	std::size_t IndexBufferPrototype32::GetElementsCount() const
+	{
+		return GeometryBufferPrototype::GetElementsCount();
+	}
+
+	std::size_t IndexBufferPrototype32::GetStride() const
+	{
+		return GeometryBufferPrototype::GetStride();
+	}
+
+	GeometryPrototype::GeometryPrototype() = default;
+	GeometryPrototype::~GeometryPrototype() = default;
+	GeometryPrototype::GeometryPrototype(GeometryPrototype&&) noexcept = default;
+	GeometryPrototype& GeometryPrototype::operator=(GeometryPrototype&&) noexcept = default;
+
 }
--- src/Core/Geometry.hpp
+++ src/Core/Geometry.hpp
@@ -1,41 +1,15 @@
 #pragma once
 #include <vector>
+#include <Core/Base.hpp>
 
 namespace MMPEngine::Core
 {
-	enum class VertexBufferFormat : std::uint8_t
-	{
-		Float1,
-		Float2,
-		Float3,
-		Float4,
-		Uint4,
-	};
-
-	enum class IndexBufferFormat : std::uint8_t
-	{
-		Uint16,
-		Uint32
-	};
-
-	enum class VertexBufferPrototypeSemantics : std::uint8_t
-	{
-		Position,
-		Color,
-		Normal,
-		BiNormal,
-		Tangent,
-		UV,
-		BlendIndices,
-		BlendWeight
-	};
-
 	class BaseGeometryBufferPrototype
 	{
 	public:
-		struct Settings
+		struct Settings final
 		{
-			//TODO: support some flags for vert/index buffers: UAV, raw...
+			//TODO: support some flags for vert/index buffers: unordered access, raw access...
 		};
 		virtual std::size_t GetStride() const = 0;
 		virtual std::size_t GetByteLength() const = 0;
@@ -46,10 +20,227 @@ namespace MMPEngine::Core
 		BaseGeometryBufferPrototype(const Settings& s);
 		BaseGeometryBufferPrototype(const BaseGeometryBufferPrototype&) = delete;
 		BaseGeometryBufferPrototype& operator=(const BaseGeometryBufferPrototype&) = delete;
-		BaseGeometryBufferPrototype(BaseGeometryBufferPrototype&&) noexcept = default;
-		BaseGeometryBufferPrototype& operator=(BaseGeometryBufferPrototype&&) noexcept = default;
+		BaseGeometryBufferPrototype(BaseGeometryBufferPrototype&&) noexcept;
+		BaseGeometryBufferPrototype& operator=(BaseGeometryBufferPrototype&&) noexcept;
 		virtual ~BaseGeometryBufferPrototype();
 	protected:
 		Settings _settings;
 	};
+
+	template<class TElement>
+	class GeometryBufferPrototype : public virtual BaseGeometryBufferPrototype
+	{
+		static_assert(std::is_pod_v<TElement>);
+	public:
+
+		GeometryBufferPrototype(const Settings& s);
+		GeometryBufferPrototype(const GeometryBufferPrototype&) = delete;
+		GeometryBufferPrototype& operator=(const GeometryBufferPrototype&) = delete;
+		GeometryBufferPrototype(GeometryBufferPrototype&&) noexcept;
+		GeometryBufferPrototype& operator=(GeometryBufferPrototype&&) noexcept;
+		~GeometryBufferPrototype() override;
+
+		const void* GetDataPtr() const override;
+		std::size_t GetStride() const override;
+		std::size_t GetByteLength() const override;
+		std::size_t GetElementsCount() const override;
+
+		std::vector<TElement> data;
+	};
+
+	class VertexBufferPrototype : public virtual BaseGeometryBufferPrototype
+	{
+	public:
+		enum class Format : std::uint8_t
+		{
+			Float1,
+			Float2,
+			Float3,
+			Float4,
+			Uint4,
+		};
+		enum class Semantics : std::uint8_t
+		{
+			Position,
+			Color,
+			Normal,
+			BiNormal,
+			Tangent,
+			UV,
+			BlendIndices,
+			BlendWeight
+		};
+		struct VBSettings final
+		{
+			Semantics semantics = Semantics::Position;
+		};
+		struct Settings final
+		{
+			VBSettings vb {};
+			BaseGeometryBufferPrototype::Settings base {};
+		};
+
+		VertexBufferPrototype(const VBSettings& vb);
+		virtual Format GetFormat() const = 0;
+		const VBSettings& GetVBSettings() const;
+
+
+	private:
+		VBSettings _vb;
+	};
+
+	class IndexBufferPrototype : public virtual BaseGeometryBufferPrototype
+	{
+	public:
+		enum class Format : std::uint8_t
+		{
+			Uint16,
+			Uint32
+		};
+		virtual Format GetFormat() const = 0;
+	};
+
+	class IndexBufferPrototype16 final : public IndexBufferPrototype, public GeometryBufferPrototype<std::uint16_t>
+	{
+	public:
+		IndexBufferPrototype16(const Settings& s);
+		Format GetFormat() const override;
+
+		std::size_t GetByteLength() const override;
+		const void* GetDataPtr() const override;
+		std::size_t GetElementsCount() const override;
+		std::size_t GetStride() const override;
+	};
+
+	class IndexBufferPrototype32 final : public IndexBufferPrototype, public GeometryBufferPrototype<std::uint32_t>
+	{
+	public:
+		IndexBufferPrototype32(const Settings& s);
+		Format GetFormat() const override;
+
+		std::size_t GetByteLength() const override;
+		const void* GetDataPtr() const override;
+		std::size_t GetElementsCount() const override;
+		std::size_t GetStride() const override;
+	};
+
+	class VertexBufferPrototypeFloat1 final : public VertexBufferPrototype, public GeometryBufferPrototype<std::float_t>
+	{
+	public:
+		explicit VertexBufferPrototypeFloat1(const Settings& settings);
+		Format GetFormat() const override;
+
+		std::size_t GetByteLength() const override;
+		const void* GetDataPtr() const override;
+		std::size_t GetElementsCount() const override;
+		std::size_t GetStride() const override;
+	};
+
+	class VertexBufferPrototypeFloat2 final : public VertexBufferPrototype, public GeometryBufferPrototype<Vector2Float>
+	{
+	public:
+		explicit VertexBufferPrototypeFloat2(const Settings& settings);
+		Format GetFormat() const override;
+
+		std::size_t GetByteLength() const override;
+		const void* GetDataPtr() const override;
+		std::size_t GetElementsCount() const override;
+		std::size_t GetStride() const override;
+	};
+
+	class VertexBufferPrototypeFloat3 final : public VertexBufferPrototype, public GeometryBufferPrototype<Vector3Float>
+	{
+	public:
+		explicit VertexBufferPrototypeFloat3(const Settings& settings);
+		Format GetFormat() const override;
+
+		std::size_t GetByteLength() const override;
+		const void* GetDataPtr() const override;
+		std::size_t GetElementsCount() const override;
+		std::size_t GetStride() const override;
+	};
+
+	class VertexBufferPrototypeFloat4 final : public VertexBufferPrototype, public GeometryBufferPrototype<Vector4Float>
+	{
+	public:
+		explicit VertexBufferPrototypeFloat4(const Settings& settings);
+		Format GetFormat() const override;
+
+		std::size_t GetByteLength() const override;
+		const void* GetDataPtr() const override;
+		std::size_t GetElementsCount() const override;
+		std::size_t GetStride() const override;
+	};
+
+	class VertexBufferPrototypeUint4 final : public VertexBufferPrototype, public GeometryBufferPrototype<Vector4Uint>
+	{
+	public:
+		explicit VertexBufferPrototypeUint4(const Settings& settings);
+		Format GetFormat() const override;
+
+		std::size_t GetByteLength() const override;
+		const void* GetDataPtr() const override;
+		std::size_t GetElementsCount() const override;
+		std::size_t GetStride() const override;
+	};
+
+	class GeometryPrototype final
+	{
+	public:
+		struct Subset final
+		{
+			std::uint32_t indexCount = 0;
+			std::uint32_t indexStart = 0;
+			std::uint32_t baseVertex = 0;
+		};
+
+		std::vector<std::unique_ptr<VertexBufferPrototype>> vertexBuffers;
+		std::unique_ptr<IndexBufferPrototype> indexBuffer;
+		std::vector<Subset> subsets;
+
+		GeometryPrototype();
+		~GeometryPrototype();
+		GeometryPrototype(const GeometryPrototype&) = delete;
+		GeometryPrototype& operator=(const GeometryPrototype&) = delete;
+		GeometryPrototype(GeometryPrototype&&) noexcept;
+		GeometryPrototype& operator=(GeometryPrototype&&) noexcept;
+	};
+
+	template<class TElement>
+	inline GeometryBufferPrototype<TElement>::GeometryBufferPrototype(const Settings& s) : BaseGeometryBufferPrototype(s)
+	{
+	}
+
+	template<class TElement>
+	inline GeometryBufferPrototype<TElement>& GeometryBufferPrototype<TElement>::operator=(GeometryBufferPrototype&&) noexcept = default;
+
+	template<class TElement>
+	inline GeometryBufferPrototype<TElement>::GeometryBufferPrototype(GeometryBufferPrototype&&) noexcept = default;
+
+	template<class TElement>
+	inline GeometryBufferPrototype<TElement>::~GeometryBufferPrototype() = default;
+
+	template<class TElement>
+	inline const void* GeometryBufferPrototype<TElement>::GetDataPtr() const
+	{
+		return data.data();
+	}
+
+	template<class TElement>
+	inline std::size_t GeometryBufferPrototype<TElement>::GetStride() const
+	{
+		return sizeof(TElement);
+	}
+
+	template<class TElement>
+	inline std::size_t GeometryBufferPrototype<TElement>::GetByteLength() const
+	{
+		return GetStride() * data.size();
+	}
+
+	template<class TElement>
+	inline std::size_t GeometryBufferPrototype<TElement>::GetElementsCount() const
+	{
+		return data.size();
+	}
 }
