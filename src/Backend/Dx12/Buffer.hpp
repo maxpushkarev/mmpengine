66c296347ef972ba92c7604b4a620620c4528aac
Максим Пушкарев <maxim.pushkarev@playrix.com>
2024-12-22 17:59:26 +0400

    constant buffer placement

--- src/Backend/Dx12/Buffer.hpp
+++ src/Backend/Dx12/Buffer.hpp
@@ -325,18 +325,11 @@ namespace MMPEngine::Backend::Dx12
 	template<class TUniformBufferData>
 	inline std::size_t UniformBuffer<TUniformBufferData>::GetRequiredSize()
 	{
-		// Constant buffers must be a multiple of the minimum hardware
-		// allocation size (usually 256 bytes).  So round up to nearest
-		// multiple of 256.  We do this by adding 255 and then masking off
-		// the lower 2 bytes which store all bits < 256.
-		// Example: Suppose byteSize = 300.
-		// (300 + 255) & ~255
-		// 555 & ~255
-		// 0x022B & ~0x00ff
-		// 0x022B & 0xff00
-		// 0x0200
-		// 512
-		return (sizeof(TUniformBufferData) + 255) & ~255;
+		const auto res = (
+			(sizeof(TUniformBufferData) + D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT - 1) /
+				D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT * D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT
+		);
+		return res;
 	}
 
 }
