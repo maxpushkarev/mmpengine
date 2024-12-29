3e0d8f2d60d3677dd22b265a21f4e73d49fd8551
Максим Пушкарев <maxim.pushkarev@playrix.com>
2024-12-29 19:45:41 +0400

    added: textures

--- src/Core/Texture.cpp
+++ src/Core/Texture.cpp
@@ -2,5 +2,23 @@
 
 namespace MMPEngine::Core
 {
+	BaseTexture::BaseTexture(const std::string& name) : BaseEntity(name)
+	{
+	}
 
+	TargetTexture::TargetTexture(const std::string& name) : BaseTexture(name)
+	{
+	}
+
+	ScreenTargetTexture::ScreenTargetTexture(const Settings& settings) : TargetTexture("screen_target_texture"), _settings(settings)
+	{
+	}
+
+	ColorTargetTexture::ColorTargetTexture(const Settings& settings) : TargetTexture(settings.base.name), _settings(settings)
+	{
+	}
+
+	DepthStencilTargetTexture::DepthStencilTargetTexture(const Settings& settings) : TargetTexture(settings.base.name), _settings(settings)
+	{
+	}
 }
--- src/Core/Texture.hpp
+++ src/Core/Texture.hpp
@@ -1,6 +1,88 @@
 #pragma once
+#include <Core/Entity.hpp>
 
 namespace MMPEngine::Core
 {
+	class BaseTexture : public BaseEntity
+	{
+	protected:
+		BaseTexture(const std::string& name);
+	};
 
+	class TargetTexture : public BaseTexture
+	{
+	public:
+		struct Settings final
+		{
+			enum class Antialiasing : std::uint8_t
+			{
+				MSAA_0,
+				MSAA_2,
+				MSAA_4,
+				MSAA_8
+			};
+
+			Antialiasing antialiasing = Antialiasing::MSAA_0;
+			Vector2Uint size{ 0,0 };
+			std::string name {};
+		};
+	protected:
+		TargetTexture(const std::string& name);
+	};
+
+	class ScreenTargetTexture : public TargetTexture
+	{
+	public:
+		struct Settings final
+		{
+			Vector2Uint size{ 0,0 };
+		};
+	protected:
+		ScreenTargetTexture(const Settings& settings);
+		Settings _settings;
+	};
+
+	class ColorTargetTexture : public TargetTexture
+	{
+	public:
+		struct Settings final
+		{
+			enum class Format : std::uint8_t
+			{
+				R8G8B8A8_Float_01,
+				R32G32B32A32_Float_Unbound,
+				R32G32B32_Float_Unbound,
+				R32G32_Float_Unbound,
+				R32_Float_Unbound
+			};
+
+			Format format = Format::R8G8B8A8_Float_01;
+			TargetTexture::Settings base;
+		};
+	protected:
+		ColorTargetTexture(const Settings& settings);
+	protected:
+		Settings _settings;
+	};
+
+	class DepthStencilTargetTexture : public TargetTexture
+	{
+	public:
+		struct Settings final
+		{
+			enum class Format : std::uint8_t
+			{
+				Depth24_Stencil8,
+				Depth16,
+				Depth32
+			};
+
+			Format format = Format::Depth24_Stencil8;
+			TargetTexture::Settings base;
+		};
+	protected:
+		DepthStencilTargetTexture(const Settings& settings);
+	protected:
+		Settings _settings;
+	};
 }
