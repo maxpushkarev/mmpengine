68cb7efac9384a8ae1870e285c4ecfc7fcf1ca31
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-01-25 22:26:54 +0400

    added: dot implementation

--- src/Backend/Shared/Math.cpp
+++ src/Backend/Shared/Math.cpp
@@ -1,5 +1,12 @@
 #include <Backend/Shared/Math.hpp>
+#include <glm/geometric.hpp>
+#include <glm/vec3.hpp>
 
 namespace MMPEngine::Backend::Shared
 {
+	std::float_t GLMMath::Dot(const Core::Vector3Float& v1, const Core::Vector3Float& v2) const
+	{
+		return glm::dot(reinterpret_cast<const glm::vec3&>(v1), reinterpret_cast<const glm::vec3&>(v2));
+	}
+
 }
--- src/Backend/Shared/Math.hpp
+++ src/Backend/Shared/Math.hpp
@@ -5,5 +5,8 @@ namespace MMPEngine::Backend::Shared
 {
 	class GLMMath final : public Core::Math
 	{
+	public:
+		std::float_t Dot(const Core::Vector3Float& v1, const Core::Vector3Float& v2) const override;
+
 	};
 }
