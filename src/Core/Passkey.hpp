742b8617a6db616d97babe72875d8ff6a2184c49
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-02-06 23:59:57 +0400

    added: passkey idiom

---
+++ src/Core/Passkey.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <type_traits>
+
+namespace MMPEngine::Core
+{
+	template<typename T>
+	class PasskeyRequest
+	{
+		friend T;
+	private:
+		explicit PasskeyRequest(const T*)
+		{
+		}
+	};
+
+	template<bool AllowInheritance, typename...Types>
+	class PasskeyControl
+	{
+	private:
+		template<typename TRequest>
+		static constexpr bool IsRequestedTypeAllowed()
+		{
+			return false;
+		}
+
+		template<typename TRequest, typename TAllowedType, typename...OtherAllowedTypes>
+		static constexpr bool IsRequestedTypeAllowed()
+		{
+			return (AllowInheritance ? std::is_base_of_v<TAllowedType, TRequest> : std::is_same_v<TAllowedType, TRequest>)
+				|| IsRequestedTypeAllowed<TRequest, OtherAllowedTypes...>();
+		}
+
+	public:
+		template<typename T>
+		PasskeyControl(const std::enable_if_t<IsRequestedTypeAllowed<T, Types...>(), PasskeyRequest<T>>&)
+		{
+		}
+	};
+}
