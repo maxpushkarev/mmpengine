0d1cacbac9c383c31d95679fa2cffdfc69e17dd3
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-02-16 17:33:58 +0400

    create init task for device memory block

--- src/Backend/Vulkan/Memory.cpp
+++ src/Backend/Vulkan/Memory.cpp
@@ -6,4 +6,20 @@ namespace MMPEngine::Backend::Vulkan
 	{
 	}
 
+	DeviceMemoryBlock::InitTask::InitTask(const std::shared_ptr<InitTaskContext>& ctx) : Task<MMPEngine::Backend::Vulkan::DeviceMemoryBlock::InitTaskContext>(ctx)
+	{
+	}
+
+	void DeviceMemoryBlock::InitTask::Run(const std::shared_ptr<Core::BaseStream>& stream)
+	{
+		Task::Run(stream);
+	}
+
+	std::shared_ptr<Core::BaseTask> DeviceMemoryBlock::CreateInitializationTask()
+	{
+		const auto ctx = std::make_shared<InitTaskContext>();
+		ctx->entity = std::dynamic_pointer_cast<DeviceMemoryBlock>(shared_from_this());
+		return std::make_shared<InitTask>(ctx);
+	}
+
 }
--- src/Backend/Vulkan/Memory.hpp
+++ src/Backend/Vulkan/Memory.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <Core/Entity.hpp>
+#include <Backend/Vulkan/Task.hpp>
 
 namespace MMPEngine::Backend::Vulkan
 {
@@ -12,7 +13,20 @@ namespace MMPEngine::Backend::Vulkan
 		};
 
 		DeviceMemoryBlock(const Settings&);
+		std::shared_ptr<Core::BaseTask> CreateInitializationTask() override;
 	private:
 		Settings _settings;
+
+		class InitTaskContext final : public Core::EntityTaskContext<DeviceMemoryBlock>
+		{
+		};
+
+		class InitTask final : public Task<InitTaskContext>
+		{
+		public:
+			InitTask(const std::shared_ptr<InitTaskContext>& ctx);
+		protected:
+			void Run(const std::shared_ptr<Core::BaseStream>& stream) override;
+		};
 	};
 }
