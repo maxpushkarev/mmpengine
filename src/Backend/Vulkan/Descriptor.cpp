875df3ea05f1831b886c396a55722174d515652b
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-04-29 18:02:07 +0400

    descriptor pool as entity

--- src/Backend/Vulkan/Descriptor.cpp
+++ src/Backend/Vulkan/Descriptor.cpp
@@ -2,5 +2,34 @@
 
 namespace MMPEngine::Backend::Vulkan
 {
+	DescriptorPool::DescriptorPool(const Settings& settings) : _settings(settings), _pool(nullptr)
+	{
+	}
 
+	DescriptorPool::~DescriptorPool()
+	{
+		if (_device && _pool)
+		{
+			vkDestroyDescriptorPool(_device->GetNativeLogical(), _pool, nullptr);
+		}
+	}
+
+	DescriptorPool::InitTask::InitTask(const std::shared_ptr<InitTaskContext>& ctx) : Task<MMPEngine::Backend::Vulkan::DescriptorPool::InitTaskContext>(ctx)
+	{
+	}
+
+	void DescriptorPool::InitTask::Run(const std::shared_ptr<Core::BaseStream>& stream)
+	{
+		Task::Run(stream);
+		const auto entity = GetTaskContext()->entity;
+		entity->_device = _specificGlobalContext->device;
+
+	}
+
+	std::shared_ptr<Core::BaseTask> DescriptorPool::CreateInitializationTask()
+	{
+		const auto ctx = std::make_shared<InitTaskContext>();
+		ctx->entity = std::dynamic_pointer_cast<DescriptorPool>(shared_from_this());
+		return std::make_shared<InitTask>(ctx);
+	}
 }
--- src/Backend/Vulkan/Descriptor.hpp
+++ src/Backend/Vulkan/Descriptor.hpp
@@ -1,7 +1,47 @@
 #pragma once
+#include <Core/Heap.hpp>
 #include <Backend/Vulkan/Wrapper.hpp>
+#include <Backend/Vulkan/Task.hpp>
 
 namespace MMPEngine::Backend::Vulkan
 {
+	class DescriptorPool final : public Core::BaseEntity
+	{
+	public:
+		struct Settings final
+		{
+			std::vector<VkDescriptorPoolSize> sizeInfos;
+		};
 
+		DescriptorPool(const Settings&);
+		DescriptorPool(const DescriptorPool&) = delete;
+		DescriptorPool(DescriptorPool&&) noexcept = delete;
+		DescriptorPool& operator=(const DescriptorPool&) = delete;
+		DescriptorPool& operator=(DescriptorPool&&) noexcept = delete;
+		~DescriptorPool() override;
+
+		std::shared_ptr<Core::BaseTask> CreateInitializationTask() override;
+
+	private:
+		std::shared_ptr<Wrapper::Device> _device;
+		Settings _settings;
+		VkDescriptorPool _pool;
+
+		class InitTaskContext final : public Core::EntityTaskContext<DescriptorPool>
+		{
+		};
+
+		class InitTask final : public Task<InitTaskContext>
+		{
+		public:
+			InitTask(const std::shared_ptr<InitTaskContext>& ctx);
+		protected:
+			void Run(const std::shared_ptr<Core::BaseStream>& stream) override;
+		};
+	};
+
+	/*class DescriptorPool final : public Core::Heap
+	{
+			
+	};*/
 }
