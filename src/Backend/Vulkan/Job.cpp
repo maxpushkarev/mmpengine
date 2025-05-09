ae2bbf91fe110c23163fbc4d4db81636553aa339
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-05-09 20:57:49 +0400

    added: job

--- src/Backend/Vulkan/Job.cpp
+++ src/Backend/Vulkan/Job.cpp
@@ -2,5 +2,54 @@
 
 namespace MMPEngine::Backend::Vulkan
 {
+	BaseJob::BaseJob() = default;
+	BaseJob::~BaseJob() = default;
 
+	BaseJob::ApplyParametersTask::ApplyParametersTask(const std::shared_ptr<TaskContext>& context) : Task<TaskContext>(context)
+	{
+		_switchState = std::make_shared<SwitchState>(context);
+		_apply = std::make_shared<Apply>(context);
+	}
+
+	void BaseJob::ApplyParametersTask::OnScheduled(const std::shared_ptr<Core::BaseStream>& stream)
+	{
+		Task::OnScheduled(stream);
+
+		stream->Schedule(_switchState);
+		stream->Schedule(_apply);
+	}
+
+	BaseJob::ApplyParametersTask::SwitchState::SwitchState(const std::shared_ptr<TaskContext>& context) : Task<TaskContext>(context)
+	{
+	}
+
+	void BaseJob::ApplyParametersTask::SwitchState::OnScheduled(const std::shared_ptr<Core::BaseStream>& stream)
+	{
+		Task::OnScheduled(stream);
+
+		if (const auto job = this->GetTaskContext()->job)
+		{
+			for (const auto& sst : job->_switchMaterialParametersStateTasks)
+			{
+				stream->Schedule(sst);
+			}
+		}
+	}
+
+	BaseJob::ApplyParametersTask::Apply::Apply(const std::shared_ptr<TaskContext>& context) : Task<TaskContext>(context)
+	{
+	}
+
+	void BaseJob::ApplyParametersTask::Apply::Run(const std::shared_ptr<Core::BaseStream>& stream)
+	{
+		Task::Run(stream);
+
+		if (const auto job = this->GetTaskContext()->job; const auto sc = _specificStreamContext)
+		{
+			for (const auto& applyParameterCallback : job->_applyMaterialParametersCallbacks)
+			{
+				applyParameterCallback(sc);
+			}
+		}
+	}
 }
--- src/Backend/Vulkan/Job.hpp
+++ src/Backend/Vulkan/Job.hpp
@@ -1,7 +1,63 @@
 #pragma once
-#include <Core/Job.hpp>
+#include <vulkan/vulkan.h>
+#include <Core/Material.hpp>
+#include <Backend/Vulkan/Context.hpp>
+#include <Backend/Vulkan/Task.hpp>
+#include <Backend/Vulkan/Entity.hpp>
 
 namespace MMPEngine::Backend::Vulkan
 {
+	class BaseJob
+	{
+	public:
+		BaseJob(const BaseJob&) = delete;
+		BaseJob(BaseJob&&) noexcept = delete;
+		BaseJob& operator=(const BaseJob&) = delete;
+		BaseJob& operator=(BaseJob&&) noexcept = delete;
 
+	protected:
+		BaseJob();
+		virtual	~BaseJob();
+
+		std::vector<std::shared_ptr<Core::BaseTask>> _switchMaterialParametersStateTasks;
+		std::vector<std::function<void(const std::shared_ptr<StreamContext>& streamContext)>> _applyMaterialParametersCallbacks;
+
+		class TaskContext final : public Core::TaskContext
+		{
+		public:
+			std::shared_ptr<BaseJob> job;
+		};
+
+		class ApplyParametersTask : public Task<TaskContext>
+		{
+		private:
+			class SwitchState final : public Task<TaskContext>
+			{
+			public:
+				SwitchState(const std::shared_ptr<TaskContext>& context);
+				void OnScheduled(const std::shared_ptr<Core::BaseStream>& stream) override;
+			};
+
+			class Apply final : public Task<TaskContext>
+			{
+			public:
+				Apply(const std::shared_ptr<TaskContext>& context);
+				void Run(const std::shared_ptr<Core::BaseStream>& stream) override;
+			};
+
+		public:
+			ApplyParametersTask(const std::shared_ptr<TaskContext>& context);
+			void OnScheduled(const std::shared_ptr<Core::BaseStream>& stream) override;
+
+		private:
+			std::shared_ptr<Core::BaseTask> _switchState;
+			std::shared_ptr<Core::BaseTask> _apply;
+		};
+	};
+
+	template<typename TCoreMaterial>
+	class Job : public BaseJob
+	{
+		static_assert(std::is_base_of_v<Core::BaseMaterial, TCoreMaterial>, "TCoreMaterial must be derived from Core::BaseMaterial");
+	};
 }
